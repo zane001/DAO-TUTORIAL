4b2b1ad8517041113f1dd5a1ccff236b6595e0b0
张猛 <zhangmeng717@zzbank.cn>
2023-04-28 17:46:12 +0800

    first commit

---
+++ backend/constants.js
@@ -0,0 +1,4 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS = "0x85861b6c884Ee577437618a333343b54E8E76efB";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
